60af2bcf8d2e91f90ee7156ae2bf529c2ef76f68
zjt0515 <876737761@qq.com>
2024-11-05 14:04:58 +0800

    20241105

---
+++ src/layouts/GlobalLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="globalLayout">
+    <n-layout>
+      <n-layout-header><GlobalHeader/></n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+        <RouterView />
+      </n-layout-content>
+      <n-layout-footer>footer</n-layout-footer>
+    </n-layout>
+    <n-layout></n-layout>
+  </div>
+</template>
+
+<script setup>
+import GlobalHeader from './global/GlobalHeader.vue';
+</script>
---
+++ src/layouts/global/GlobalHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <n-menu :options="menuOptions" @update:value="handleUpdateValue" />
+</template>
+
+<script lang="ts">
+import type { Component } from "vue";
+import { defineComponent, h } from "vue";
+import { NIcon, useMessage } from "naive-ui";
+import type { MenuOption } from "naive-ui";
+import { RouterLink } from "vue-router";
+
+const menuOptions: MenuOption[] = [
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: "home",
+            params: {
+              lang: "zh-CN",
+            },
+          },
+        },
+        { default: () => "回家" }
+      ),
+    key: "go-back-home",
+  },
+  {
+    key: "divider-1",
+    type: "divider",
+    props: {
+      style: {
+        marginLeft: "32px",
+      },
+    },
+  },
+  {
+    label: () =>
+      h(
+        "a",
+        {
+          href: "https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F",
+          target: "_blank",
+          rel: "noopenner noreferrer",
+        },
+        "且听风吟"
+      ),
+    key: "hear-the-wind-sing",
+  },
+  {
+    label: "C语言括号配对",
+    key: "pinball-1973",
+  },
+  {
+    label: "二叉树寻找祖先",
+    key: "a-wild-sheep-chase",
+  },
+];
+
+export default defineComponent({
+  setup() {
+    const message = useMessage();
+    return {
+      menuOptions,
+      handleUpdateValue(key: string, item: MenuOption) {
+        message.info(`[onUpdate:value]: ${JSON.stringify(key)}`);
+        message.info(`[onUpdate:value]: ${JSON.stringify(item)}`);
+      },
+    };
+  },
+});
+</script>
